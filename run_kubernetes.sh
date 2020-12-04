0300c125257aade8cc755fd5181ce155e3daa300
Tugce Ozberk Yener <tugce.ozberkyener@shell.com>
2020-12-04 11:15:44 +0000

    Task 6: Deploy with Kubernetes and Save Output Logs

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=tugceozberkyener/udacitymicroservicesprj:latest
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+# kubectl run udacitymicroservicesprj --image=$dockerpath --port=80
+kubectl run --generator=run-pod/v1 --image=$dockerpath udacitypredictions
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward udacitypredictions 8000:80
